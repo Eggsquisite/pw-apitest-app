e3fca93340dcebfcda9bf7a00d63d74e4918a1f4
Marc Sevilleja <marc.sevilleja@gmail.com>
2025-03-14 21:51:02 -0700

    Initial commit

---
+++ tests/workingWithAPI.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({page}) => {
+  await page.goto('https://conduit.bondaracademy.com/');
+});
+
+test('has title', async ({ page }) => {
+  // Expect a title "to contain" a substring.
+  expect(await page.locator('.navbar-brand').textContent()).toEqual('conduit');
+  await expect(page.locator('.navbar-brand')).toHaveText('conduit');
+});
